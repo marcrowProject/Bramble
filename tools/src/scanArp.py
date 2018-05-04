3c9ab039f781f888c7374cdb069cc5be7f7f0da6
Marcrow <marcrowProject@gmail.com>
2018-05-04 16:52:46 +0200

    Add a decoy scan option
    -add target's hostname

--- tools/src/scanArp.py
+++ tools/src/scanArp.py
@@ -2,11 +2,26 @@
 
 #sudo apt-get install python-netifaces python-netaddr
 from scapy.all import * #Forge packets
+from threading import Thread #Use thread for decoy scan
 import netifaces #Manipulate interfaces
 import netaddr   #Manipulate address
+import random
 import os
 import sys
 import argparse
+import time
+
+class Decoy(Thread):
+    def __init__(self, fakeIp, targetList, interface):
+        Thread.__init__(self)
+        self.fakeIp = fakeIp
+        self.targetList = targetList
+        self.interface = interface
+
+    def run(self):
+        send(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = self.targetList, psrc=self.fakeIp), iface=self.interface, verbose=0)
+
+
 
 class colors: #Add colors in terminal
     HEADER = '\033[95m'
@@ -26,9 +41,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--output",
                     help="Output file,default=./result/scanNetwork/scanARP",
                     default="./result/scanNetwork/scanARP")
+parser.add_argument("-d", "--decoy",
+                    help="It doesn't hide your own IP, but it makes your IP one of a torrent of others supposedly scanning the victim at the same time",
+                    action="store_true")
 parser.add_argument("-v", "--verbose",
-                    help="Verbosity, if set on 0 you have no information, on 1 you have info when it start and stop emission, on 2 when it have a response",
-                    type=int, default=2)
+                    help="Verbosity, if set on 0 you have no information, on 1 you have info when it start and stop emission, on 2 when there is a response",
+                    type=int, default=1)
 args = parser.parse_args()
 
 my_output = args.output
@@ -72,16 +90,38 @@ for address in netaddr_list:
 #Start the ARP scan
 if verbosity > 0:
     print(colors.OKGREEN+"Be patient that can take a while."+colors.ENDC)
-ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = addr_list), timeout=1, iface=my_interface, retry=2, verbose=verbosity)
-
+#send a first random decoy scan if u use the option
+if args.decoy:
+    if verbosity > 0:
+        print("send a first decoy")
+    fake = random.randint(0,len(addr_list))
+    thread_1 = Decoy(str(fake), addr_list, my_interface)
+    thread_1.start()
+    thread_1.join()
+    time.sleep(0.3)
+#send&receive packets for the arp scan
+ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = addr_list), timeout=1, iface=my_interface, verbose=verbosity) #retry=2
 #-----------------------------Save the result------------------------------------------
 try:
     my_file = open(my_output, "w")
+    nb_decoy=3 # limit the number of decoys
+
     for snd, rcv in ans:
+        his_ip = rcv.sprintf(r"%Ether.psrc%")
+        his_mac = rcv.sprintf(r"%Ether.src%")
+        thread_1 = Decoy(str(his_ip), addr_list, my_interface)
+        if args.decoy and nb_decoy > 0:
+            thread_1.start()
+        info = socket.gethostbyaddr(his_ip)
+        his_hostname = str(info[0])
         if verbosity > 0:
-            print rcv.sprintf(r"%Ether.src% - %Ether.psrc%")
-        my_file.write(rcv.sprintf(r"%Ether.psrc%"))
+            print(his_mac+" - "+his_ip+" - "+his_hostname)
+        my_file.write(his_ip)
         my_file.write("\n")
+        if args.decoy and nb_decoy > 0:
+            thread_1.join()
+            nb_decoy -= 1
+
     if verbosity > 0:
         print(colors.OKGREEN+"\nIt's done."+colors.ENDC+" Press a button to quit")
 except IOError:
