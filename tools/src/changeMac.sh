ff267974578b8462ed9e56fa9d7e06ef4731e61c
Marcrow <marcrowProject@gmail.com>
2018-05-14 15:04:23 +0200

    improve the user interface

--- tools/src/changeMac.sh
+++ tools/src/changeMac.sh
@@ -3,22 +3,30 @@
 transparent="\e[0m"
 bReverse="\e[7;1m"
 title="\e[3;33m"
+green="\033[1;32m"
+
+clear
 
 ans="n"
 allInterface=$(ip link show | grep '^[1-9]' | cut -d ' ' -f 2  | cut -d ":" -f -1 | grep -v "lo")
-echo -e $title" choose the interface :"$transparent
+
 while [ $ans != "y" ]
 do
 	for interface in $allInterface
 		do
+		clear
+		echo -e $title" choose the interface :"$transparent
+		echo "in "$allInterface
 		echo -e $bReverse""$interface" (y/n)"$transparent
 		read ans
 		if [ $ans = "y" ]; then
+			clear
 			sudo macchanger -s $interface
-			echo "y to change for a random Mac adress"
+			echo -e $title"y to change for a random Mac adress"
 			echo "n to quit"
-			echo "press another key to enter by yourself the mac address"
+			echo -e "press another key to enter by yourself the mac address"$transparent
 			read ans
+			clear
 			if [ $ans != "n" ]; then
 				sudo ip link set dev $interface down
 				if [ $ans = "y" ]; then
@@ -31,7 +39,7 @@ do
 				sudo ip link set dev $interface up
 				echo "wait your interface restart..."
 				sudo ./option/src/restartNetwork.sh $interface
-				echo "It's done!"
+				echo -e $green"It's done!"$transparent
 				read ans
 				exit
 			else
