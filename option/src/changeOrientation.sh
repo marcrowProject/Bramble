5c8786f89478523bbfd251ca2d35dd6c51df645a
Marcrow <marcrowProject@gmail.com>
2018-06-06 17:01:22 +0200

    adds colors and messages to prevent misuse

--- option/src/changeOrientation.sh
+++ option/src/changeOrientation.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
 
-echo "Press y to switch in right-hander mode"
-echo "Press n to switch in left-hander mode"
+transparent="\e[0m"
+bReverse="\e[7;1m"
+title="\e[3;33m"
+warning="\033[1;31m"
+
+clear
+echo -e $title"Press y to switch in right-hander mode"
+echo -e "Press n to switch in left-hander mode"$transparent
 read ans
+clear
 if [ $ans == "y" ]; then
-	sed -i 's/k1=24/k1=18/g' conf/rpi-2.2TFT-kbrd.py
-	sed -i 's/k3=18/k3=24/g' conf/rpi-2.2TFT-kbrd.py
-	cd LCD-show
-	./LCD32-show 180
+	echo -e $warning"Your device will reboot"
+	echo "please don't shutdown the device"
+	echo "during the process (it may take few minutes)."
+	echo -e "because it can damage your bramble"$transparent
+	echo "press y if you have understood"
+	read ans
+	if [ $ans == "y" ]; then
+		sed -i 's/k1=24/k1=18/g' conf/rpi-2.2TFT-kbrd.py
+		sed -i 's/k3=18/k3=24/g' conf/rpi-2.2TFT-kbrd.py
+		cd LCD-show
+		./LCD32-show 180
+	else
+		clear
+		echo -e $title"you stopped the process"
+		echo -e "press a button to quit"$transparent
+		read ans
+	fi
 else
-	sed -i 's/k1=18/k1=24/g' conf/rpi-2.2TFT-kbrd.py
-	sed -i 's/k3=24/k3=18/g' conf/rpi-2.2TFT-kbrd.py
-	cd LCD-show
-	./LCD32-show 180
+	echo -e $warning"Your device will reboot"
+	echo "please don't shutdown the device"
+	echo "during the process (it may take few minutes)."
+	echo -e "because it can damage your bramble"$transparent
+	echo "press y if you have understood"
+	read ans
+	if [ $ans == "y" ]; then
+		sed -i 's/k1=18/k1=24/g' conf/rpi-2.2TFT-kbrd.py
+		sed -i 's/k3=24/k3=18/g' conf/rpi-2.2TFT-kbrd.py
+		cd LCD-show
+		./LCD32-show 0
+	else
+		clear
+		echo -e $title"you stopped the process"
+		echo -e "press a button to quit"$transparent
+		read ans
+	fi
 fi
--- option/src/hdmi.sh
+++ option/src/hdmi.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-echo "Press y to use only the hdmi"
+transparent="\e[0m"
+bReverse="\e[7;1m"
+title="\e[3;33m"
+warning="\033[1;31m"
+green="\033[1;32m"
+
+clear
+echo -e $title"Press y to use only the hdmi"
 echo "Press n to use only the built-in screen"
-echo "Press stop to quit"
+echo -e "Press stop to quit"$transparent
 read ans
 clear
-if [ $ans == "y" ]; then 
-	echo "Be carefull the built-in screen will be disabled"
+if [ $ans == "y" ]; then
+	echo -e $warning"Be carefull the built-in screen will be disabled"
 	echo "So if you are not plugged in hdmi you will not be"
 	echo "able to use bramble."
 	echo "Press y if you are plugged in hdmi"
-	echo "Else press n without issue"
+	echo -e "Else press n without issue"$transparent
 	read ans
 	if [ $ans == "y" ]; then
-		cd LCD-show
-		./LCD-hdmi
+		clear
+		echo -e $warning"Your device will reboot"
+		echo "please don't shutdown the device"
+		echo "during the process (it may take few minutes)."
+		echo -e "because it can damage your bramble"$transparent
+		echo "press y if you have understood"
+		read ans
+		if [ $ans == "y" ]; then
+			cd LCD-show
+			./LCD-hdmi
+		else
+			clear
+			echo -e $title"you stopped the process"
+			echo -e "press a button to quit"$transparent
+			read ans
+		fi
 	fi
 else
 	./option/src/changeOrientation.sh
