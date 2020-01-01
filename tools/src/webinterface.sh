798d09343c8c9a3c2cc2ff3fc4a373def5cd737d
marcrow <MarcrowProject@gmail.com>
2020-01-01 16:50:11 +0100

    :bug: fix ip address and test if the web service is reachable

--- tools/src/webinterface.sh
+++ tools/src/webinterface.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 websitePort=443
 webconsolePort=4443
-fileviewerPort=80
 
-addr="192.168.1.77"
-
+addr_list=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 transparent="\e[0m"
 title="\e[3;33m"
 green="\033[1;32m"
@@ -18,28 +16,19 @@ if [ $1 = "--start" ]; then
 	echo -e "$title\tStart the web interface...$transparent"
     /var/www/bramble-dashboard/start.sh
     if [ $? -ne 0 ]; then
+	echo $?
         echo -e "$error Unable to start the web service or the web console"
     fi
-    status=$(nc -zv $addr $websitePort 2> /dev/null)
-    if [ $? -eq 0 ]; then
-        echo -e "$ok Web interface reachable"
-    else
-        echo -e "$error Web interface unreachable"
-    fi
+    for addr in $addr_list; do
+		status=$(nc -zv $addr $websitePort 2> /dev/null)
+		if [ $? -eq 0 ]; then
+		    echo -e "$ok Web interface reachable at $addr"
+		else
+		    echo -e "$error Web interface unreachable at $addr"
+		fi
+	done
+    
 
-    status=$(nc -zv $addr $webconsolePort 2> /dev/null)
-    if [ $? -eq 0 ]; then
-        echo -e "$ok Web console reachable"
-    else
-        echo -e "$error Web console unreachable"
-    fi
-
-    status=$(nc -zv $addr $fileviewerPort 2> /dev/null)
-    if [ $? -eq 0 ]; then
-        echo -e "$ok Web file viewer reachable"
-    else
-        echo -e "$error Web file viewer unreachable"
-    fi
 fi
 
 if [ $1 = "--stop" ]; then
